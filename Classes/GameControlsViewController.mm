b94c625cc878f40b7bbbc9afbd4786e36279a266
Stuart Carnie <stuart.carnie@gmail.com>
2009-03-03 21:11:31 -0700

    Update source headers to include GPL v3 license for iPhone specific files

--- Classes/GameControlsViewController.mm
+++ Classes/GameControlsViewController.mm
@@ -1,10 +1,21 @@
-//
-//  GameControlsViewController.mm
-//  Another World
-//
-//  Created by Stuart Carnie on 1/4/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/* 
+ Flashback for iPhone - Flashback interpreter
+ Copyright (C) 2009 Stuart Carnie
+ See gpl.txt for license information.
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import "GameControlsViewController.h"
 #import "systemstub.h"
